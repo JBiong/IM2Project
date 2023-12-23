e33909ab0f44c4be9088cebf48296a0030818a44
larapable <lara.pable@cit.edu>
2023-12-23 13:43:02 +0800

    final

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import os
-from flask import Flask,jsonify,request,render_template,flash,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
-from flask_sqlalchemy import SQLAlchemy
 from users import create_user,authenticate_user,get_users,get_user
-from orders import get_all_products,create_order
-from database import set_mysql
+from orders import add_to_cart, edit_cart, view_cart, delete_cart_item  
+from database import set_mysql, get_cursor, get_connection
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 app = Flask(__name__)
@@ -35,12 +34,12 @@ app.static_folder = "static"
 def home():
      return render_template("login.html")
 
+
 @app.route("/login", methods=["POST"])
 def login():
     if request.method == "POST":
         data = request.get_json()
         user = authenticate_user(data["email"], data["pass"])
-
         if user is not None:
             # Authentication successful
              response_data = {"id": user, "redirect_url": url_for('dashboard')}
@@ -53,6 +52,7 @@ def login():
         users = get_users()
         return jsonify(users)
 
+
 @app.route("/signup", methods=["GET","POST"])
 def signup():
     if request.method == "POST":
@@ -62,8 +62,8 @@ def signup():
     else:
         users = get_users()
         return jsonify(users)
-
     return render_template("login.html")
+
 
 @app.route("/signup/<int:id>", methods=["GET"])
 def user(id):
@@ -75,43 +75,212 @@ def user(id):
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route('/products')
-def get_products():
-    products = get_all_products()
 
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    # Use flask_mysqldb to execute the SQL query
+    cursor = get_cursor()
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    cursor.close()
+
+    # Convert the products to a JSON format and return as a response
     product_list = [
         {
             'id': product['id'],
             'category': product['category'],
             'name': product['product_name'],
-            'stock': product['product_stock'],
-            'price': float(product['price']),
+            'price': float(product['price']),  # Convert Decimal to float for JSON serialization
             'image_name': product['image_name']
         }
         for product in products
     ]
 
-    if request.headers.get('Content-Type') == 'application/json':
-        return jsonify(products=product_list)
-    else:
-        return render_template("index.html", products=product_list)
+    return render_template("index.html")
+
+@app.route('/add_to_cart_item', methods=['POST'])
+def add_to_cart_routes():
+    data = request.get_json()
+
+    items = data.get('items', [])
+    for item in items:
+        product_id = item.get('product_id')
+        size = item.get('size')
+        quantity = item.get('quantity')
+
+        if product_id and size and quantity:
+            add_to_cart(product_id, size, quantity)
+
+    # Fetch all orders from the database after the update
+    orders = view_cart()
+    total_amount = sum(order['price'] * order['quantity'] for order in orders)
+
+    return jsonify({'orders': orders, 'total_amount': total_amount})
 
 @app.route('/add_to_cart', methods=['POST'])
-def add_to_cart():
-    try:
-        data = request.get_json()
-        product_id = data.get('product_id')
-        quantity = data.get('quantity')
-        size = data.get('size')
+def add_to_cart_route():
 
-      
-        if product_id is not None and quantity is not None and size is not None:
-            # Call your create_order function here
-            order_id = create_order(product_id, quantity, size)
-            print("ID:",order_id)
-            return jsonify({'status': 'success', "order_id": order_id})
-        else:
-            return jsonify({'status': 'error', 'message': 'Invalid data format'})
+    total_products = 30  # Assuming you have 30 products
 
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+    # Fetch all orders and total amount from the database
+    orders = view_cart()  # Use the view_cart function
+
+    # Calculate the total amount
+    total_amount = sum(order['price'] * order['quantity'] for order in orders)
+
+    for dress_number in range(1, total_products + 1):
+        product_id = request.form.get(f'product_id{dress_number}')
+        size = request.form.get(f'size{dress_number}', '')
+
+        # Check if the product is selected
+        if not product_id:
+            continue
+
+        try:
+            product_id = int(product_id)
+        except ValueError:
+            return redirect(url_for('index'))
+
+        quantity = request.form.get(f'quantity{dress_number}', 0)
+
+        try:
+            quantity = int(quantity)
+        except ValueError:
+            return redirect(url_for('index'))
+
+        if quantity > 0:
+            # Check if the product is already in the cart
+            cursor = get_cursor()
+            cursor.execute("SELECT id FROM cart_items WHERE product_id = %s AND size = %s", (product_id, size))
+            existing_cart_item = cursor.fetchone()
+
+            if existing_cart_item:
+                # Product already in the cart, update the quantity
+                cursor.execute("UPDATE cart_items SET quantity = quantity + %s WHERE id = %s", (quantity, existing_cart_item['id']))
+            else:
+                # Product not in the cart, insert a new entry
+                cursor.execute("INSERT INTO cart_items (product_id, size, quantity) VALUES (%s, %s, %s)", (product_id, size, quantity))
+
+            cursor.close()
+
+    # Check if the client accepts HTML
+    if 'text/html' in request.accept_mimetypes:
+        return render_template('orderlist.html', orders=orders, total_amount=total_amount)
+    else:
+        # Return a JSON response
+        return jsonify({'orders': orders, 'total_amount': total_amount})
+
+@app.route('/edit_cart', methods=['GET', 'POST'])
+def edit_cart_route():
+
+    if request.method == 'POST':
+        # Initialize cursor before using it
+        cursor = get_cursor()
+
+        # Handle the form submission to update cart items
+        for item_id, new_value in request.form.items():
+            # Each form field is an item_id_quantity or item_id_size, and the corresponding value is the new quantity or size
+
+            try:
+                item_id, field = item_id.split('_')
+                item_id = int(item_id)
+            except ValueError:
+                # Handle invalid input
+                return redirect(url_for('edit_cart_route'))
+
+            if field == 'delete' and new_value == '1':
+                # Use the stored procedure for deletion
+                delete_cart_item(item_id)
+
+            elif field == 'quantity':
+                # Handle quantity updates using POST method
+
+                # Get the updated quantity from the form
+                new_quantity = request.form.get(f'{item_id}_quantity', 0)
+
+                try:
+                    new_quantity = int(new_quantity)
+                except ValueError:
+                    # Handle invalid input
+                    return redirect(url_for('edit_cart_route'))
+
+                cursor.execute("UPDATE cart_items SET quantity = %s WHERE id = %s", (new_quantity, item_id))
+                cursor.execute("UPDATE orders SET quantity = %s WHERE id = %s", (new_quantity, item_id))
+
+            elif field == 'size':
+                # Handle size updates using POST method
+
+                # Get the updated size from the form
+                new_size = request.form.get(f'{item_id}_size', '')
+                cursor.execute("UPDATE cart_items SET size = %s WHERE id = %s", (new_size, item_id))
+                cursor.execute("UPDATE orders SET size = %s WHERE id = %s", (new_size, item_id))
+
+        # Commit changes to the database
+        get_connection().commit()
+        cursor.close()
+
+    # Fetch all cart items from the database
+    cursor = get_cursor()
+    cursor.execute("SELECT cart_items.id, cart_items.product_id, cart_items.size, cart_items.quantity, "
+                   "products.product_name, products.price, products.image_name "
+                   "FROM cart_items JOIN products ON cart_items.product_id = products.id")
+    cart_items = cursor.fetchall()
+    cursor.close()
+
+    # Check if the client accepts HTML
+    if 'text/html' in request.accept_mimetypes:
+        return render_template('edit_cart.html', cart_items=cart_items)
+    else:
+        # Return a JSON response
+        return jsonify({'cart_items': cart_items})
+
+@app.route('/edit_cart_item', methods=['POST'])
+def edit_cart_routes():
+    data = request.get_json()
+
+    items = data.get('items', [])
+    for item in items:
+        item_id = item.get('item_id')
+        quantity = item.get('quantity')
+        size = item.get('size')
+
+        if item_id:
+            if 'delete' in item and item['delete']:
+                delete_cart_item(item_id)
+            else:
+                if quantity:
+                    # Update quantity in cart_items table
+                    cursor = get_cursor()
+                    cursor.execute("UPDATE cart_items SET quantity = %s WHERE id = %s", (quantity, item_id))
+                    cursor.close()
+
+                if size:
+                    # Update size in cart_items table
+                    cursor = get_cursor()
+                    cursor.execute("UPDATE cart_items SET size = %s WHERE id = %s", (size, item_id))
+                    cursor.close()
+
+    # Fetch all cart items from the database after the update
+    cursor = get_cursor()
+    cursor.execute("SELECT cart_items.id, cart_items.product_id, cart_items.size, cart_items.quantity, "
+                   "products.product_name, products.price, products.image_name "
+                   "FROM cart_items JOIN products ON cart_items.product_id = products.id")
+    cart_items = cursor.fetchall()
+    cursor.close()
+
+    return jsonify({'cart_items': cart_items})
+
+@app.route('/view_cart')
+def view_cart_route():
+    # Fetch all orders using the view_cart function
+    orders = view_cart()
+
+    # Calculate the total amount
+    total_amount = sum(order['price'] * order['quantity'] for order in orders)
+
+    # Check if the client accepts HTML
+    if 'text/html' in request.accept_mimetypes:
+        return render_template('orderlist.html', orders=orders, total_amount=total_amount)
+    else:
+        # Return a JSON response
+        return jsonify({'orders': orders, 'total_amount': total_amount})
--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
+
+CREATE SCHEMA `amlshop` ;
+
+-- TABLES
+
 CREATE TABLE `persons` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
@@ -25,33 +30,99 @@ u.password
 FROM persons p
 INNER JOIN users u ON u.person_id = p.id;
 
-CREATE VIEW get_orders AS 
+CREATE TABLE `products` (
+  `id` INT NOT NULL,
+  `product_name` VARCHAR(45) NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  `image_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `cart_items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NULL,
+  `size` VARCHAR(45) NULL,
+  `quantity` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `product_id_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `amlshop`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE orders (
+  id int NOT NULL AUTO_INCREMENT,
+  product_id int NOT NULL,
+  size varchar(45) NOT NULL,
+  quantity int NOT NULL,
+  PRIMARY KEY (id),
+  KEY product_id (product_id),
+  CONSTRAINT orders_ibfk_1 FOREIGN KEY (product_id) REFERENCES products (id)
+) ENGINE=InnoDB;
+
+INSERT INTO products (id, category, product_name, price, image_name)
+VALUES 
+  (1, 'Dress', 'Celestial Charm Dress', 80.00, 'dress1.png'),
+  (2, 'Dress', 'Midnight Bloom Gown', 70.00, 'dress2.png'),
+  (3, 'Dress', 'Enchanting Ember Dress', 90.00, 'dress3.png'),
+  (4, 'Dress', 'Serene Serendipity Frock', 135.00, 'dress4.png'),
+  (5, 'Dress', 'Velvet Vortex Ensemble', 95.00, 'dress5.png'),
+  (6, 'Dress', 'Radiant Rainforest Robe', 140.00, 'dress6.png'),
+  (7, 'Pants', 'Stealthy Shadow Trousers', 180.00, 'pants1.png'),
+  (8, 'Pants', 'Electric Ember Denim', 190.00, 'pants2.png'),
+  (9, 'Pants', 'Cosmic Cascade Pants', 165.00, 'pants3.png'),
+  (10, 'Pants', 'Urban Utopia Pants', 155.00, 'pants4.png'),
+  (11, 'Pants', 'Crimson Comet Pants', 170.00, 'pants5.png'),
+  (12, 'Pants', 'Sapphire Skyline', 165.00, 'pants6.png'),
+  (13, 'Skirts', 'Mystic Meadow Skirt', 110.00, 'skirt1.png'),
+  (14, 'Skirts', 'Electric Elegance Midi', 115.00, 'skirt2.png'),
+  (15, 'Skirts', 'Whimsical Waterfall Wrap', 105.00, 'skirt3.png'),
+  (16, 'Skirts', 'Enigmatic Envy', 125.00, 'skirt4.png'),
+  (17, 'Skirts', 'Crystal Cascade Skirt', 135.00, 'skirt5.png'),
+  (18, 'Skirts', 'Sunlit Serenade Pleated', 130.00, 'skirt6.png'),
+  (19, 'Tops', 'Stellar Symphony', 50.00, 'top1.png'),
+  (20, 'Tops', 'Mirage Melody', 40.00, 'top2.png'),
+  (21, 'Tops', 'Enchanted Echo Peplum', 60.00, 'top3.png'),
+  (22, 'Tops', 'Midnight Muse', 55.00, 'top4.png'),
+  (23, 'Tops', 'Voyage Camisole', 65.00, 'top5.png'),
+  (24, 'Tops', 'Celestial Serenity', 70.00, 'top6.png'),
+  (25, 'Jackets', 'Celestial Cascade', 200.00, 'jacket1.png'),
+  (26, 'Jackets', 'Arctic Aurora', 250.00, 'jacket2.png'),
+  (27, 'Jackets', 'Velvet Vortex', 230.00, 'jacket3.png'),
+  (28, 'Jackets', 'Ember Parka', 240.00, 'jacket4.png'),
+  (29, 'Jackets', 'Urban Utopia', 280.00, 'jacket5.png'),
+  (30, 'Jackets', 'Brooklyn New York', 300.00, 'jacket6.png');
+
+--VIEWS
+
+
+CREATE VIEW get_users AS 
 SELECT
-p.product_name,
-p.product_stock,
-p.price,
-p.category,
-p.image_name
-o.id,
-o.order_name,
-o.quantity,
-o.size,
+p.name,
+p.age,
+p.email,
 u.id,
 u.username,
 u.password
-FROM products p
-INNER JOIN orders o ON o.product_id = p.id INNER JOIN
-users u ON o.user_id = u.id;
+FROM persons p
+INNER JOIN users u ON u.person_id = p.id;
+
+CREATE VIEW view_cart AS
+SELECT
+    cart_items.id,
+    cart_items.product_id,
+    cart_items.size,
+    cart_items.quantity,
+    products.product_name,
+    products.price,
+    products.image_name
+FROM cart_items
+JOIN products ON cart_items.product_id = products.id;
 
 
-CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `product_id` int NOT NULL,
-  `order_name` varchar(200) NOT NULL,
-  `quantity` varchar(200) NOT NULL,
-  `size` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+--STORED PROCEDURES
+
 
 DELIMITER $$
 CREATE PROCEDURE create_user(
@@ -70,118 +141,125 @@ SELECT LAST_INSERT_ID() AS id;
 END$$
 DELIMITER;
 
+
 DELIMITER $$
 CREATE PROCEDURE create_order(
-  IN p_product_name VARCHAR(200),
-  IN p_product_stock INT,
-  IN p_price DECIMAL(10,2),
-  IN p_category VARCHAR(200),
-  IN p_image_name VARCHAR(200),
-  IN p_order_name VARCHAR(200),
-  IN p_quantity INT,
-  IN p_size VARCHAR(200),
-  IN p_username VARCHAR(200),
-  IN p_password VARCHAR(200)
+  IN p_product_name varchar(200),
+  IN p_price decimal(10,2),
+  IN p_category varchar(200),
+  IN p_image_name varchar(200),
+  IN p_size varchar(45),
+  IN p_quantity int
 )
 BEGIN
   DECLARE v_product_id INT;
-  DECLARE v_user_id INT;
 
-
-  INSERT INTO products(product_name, product_stock, price, category, image_name) 
-  VALUES(p_product_name, p_product_stock, p_price, p_category, p_image_name);
+  INSERT INTO products(product_name, price, category, image_name) 
+  VALUES(p_product_name, p_price, p_category, p_image_name);
+  
   SET v_product_id = LAST_INSERT_ID();
   
-  SELECT id INTO v_user_id FROM users WHERE username = p_username AND password = p_password;
-
-  INSERT INTO orders(product_id, user_id, order_name, quantity, size) 
-  VALUES(v_product_id, v_user_id, p_order_name, p_quantity, p_size);
-
+  INSERT INTO cart_items(product_id, size, quantity) 
+  VALUES(v_product_id, p_size, p_quantity);
+  
   SELECT LAST_INSERT_ID() AS id;
 END$$
 DELIMITER ;
 
 
-CREATE TABLE `products` (
-  `id` INT NOT NULL,
-  `product_name` VARCHAR(45) NOT NULL,
-  `product_stock` INT NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  `image_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+DELIMITER $$
+CREATE PROCEDURE update_order(
+  IN order_id int,
+  IN p_product_name varchar(200),
+  IN p_price decimal(10,2),
+  IN p_category varchar(200),
+  IN p_image_name varchar(200),
+  IN p_size varchar(45),
+  IN p_quantity int
+)
+BEGIN
+  UPDATE products
+  SET product_name = p_product_name, 
+      price = p_price, 
+      category = p_category, 
+      image_name = p_image_name
+  WHERE id = (SELECT product_id FROM cart_items WHERE id = order_id);
 
-INSERT INTO products (id, category, product_name,product_stock, price, image_name)
-VALUES 
-  (1, 'Dress', 'Celestial Charm Dress', 30, 80.00, 'dress1.png'),
-  (2, 'Dress', 'Midnight Bloom Gown', 30, 70.00, 'dress2.png'),
-  (3, 'Dress', 'Enchanting Ember Dress', 30, 90.00, 'dress3.png'),
-  (4, 'Dress', 'Serene Serendipity Frock', 30, 135.00, 'dress4.png'),
-  (5, 'Dress', 'Velvet Vortex Ensemble', 30, 95.00, 'dress5.png'),
-  (6, 'Dress', 'Radiant Rainforest Robe', 30, 140.00, 'dress6.png'),
-  (7, 'Pants', 'Stealthy Shadow Trousers', 30, 180.00, 'pants1.png'),
-  (8, 'Pants', 'Electric Ember Denim', 30, 190.00, 'pants2.png'),
-  (9, 'Pants', 'Cosmic Cascade Pants', 30, 165.00, 'pants3.png'),
-  (10, 'Pants', 'Urban Utopia Pants', 30, 155.00, 'pants4.png'),
-  (11, 'Pants', 'Crimson Comet Pants', 30, 170.00, 'pants5.png'),
-  (12, 'Pants', 'Sapphire Skyline', 30, 165.00, 'pants6.png'),
-  (13, 'Skirts', 'Mystic Meadow Skirt', 30, 110.00, 'skirt1.png'),
-  (14, 'Skirts', 'Electric Elegance Midi', 30, 115.00, 'skirt2.png'),
-  (15, 'Skirts', 'Whimsical Waterfall Wrap', 30, 105.00, 'skirt3.png'),
-  (16, 'Skirts', 'Enigmatic Envy', 30, 125.00, 'skirt4.png'),
-  (17, 'Skirts', 'Crystal Cascade Skirt', 30, 135.00, 'skirt5.png'),
-  (18, 'Skirts', 'Sunlit Serenade Pleated', 30, 130.00, 'skirt6.png'),
-  (19, 'Tops', 'Stellar Symphony', 30, 50.00 'top1.png'),
-  (20, 'Tops', 'Mirage Melody', 30, 40.00, 'top2.png'),
-  (21, 'Tops', 'Enchanted Echo Peplum', 30, 60.00, 'top3.png'),
-  (22, 'Tops', 'Midnight Muse', 30, 55.00, 'top4.png'),
-  (23, 'Tops', 'Voyage Camisole', 30, 65.00, 'top5.png'),
-  (24, 'Tops', 'Celestial Serenity', 30, 70.00, 'top6.png'),
-  (25, 'Jackets', 'Celestial Cascade', 30, 200.00, 'jacket1.png'),
-  (26, 'Jackets', 'Arctic Aurora', 30, 250.00, 'jacket2.png'),
-  (27, 'Jackets', 'Velvet Vortex', 30, 230.00, 'jacket3.png'),
-  (28, 'Jackets', 'Ember Parka', 30, 240.00, 'jacket4.png'),
-  (29, 'Jackets', 'Urban Utopia', 30, 280.00, 'jacket5.png'),
-  (30, 'Jackets', 'Brooklyn New York', 30, 300.00, 'jacket6.png');
+  UPDATE cart_items
+  SET size = p_size, quantity = p_quantity
+  WHERE id = order_id;
+
+  SELECT order_id AS id;
+END$$
+DELIMITER ;
 
 
+DELIMITER $$
+CREATE PROCEDURE delete_order(IN order_id int)
+BEGIN
+  DECLARE v_product_id INT;
 
--- DELIMITER $$
--- CREATE PROCEDURE update_order(
---   IN order_id int,
---   IN p_product_name varchar(200),
---   IN p_price decimal(10,2),
---   IN p_category varchar(200),
---   IN p_image_name varchar(200),
---   IN p_size varchar(45),
---   IN p_quantity int
--- )
--- BEGIN
---   UPDATE products
---   SET product_name = p_product_name, 
---       price = p_price, 
---       category = p_category, 
---       image_name = p_image_name
---   WHERE id = (SELECT product_id FROM cart_items WHERE id = order_id);
+  SELECT product_id INTO v_product_id FROM cart_items WHERE id = order_id;
 
---   UPDATE cart_items
---   SET size = p_size, quantity = p_quantity
---   WHERE id = order_id;
-
---   SELECT order_id AS id;
--- END$$
--- DELIMITER ;
-
--- DELIMITER $$
--- CREATE PROCEDURE delete_order(IN order_id int)
--- BEGIN
---   DECLARE v_product_id INT;
-
---   SELECT product_id INTO v_product_id FROM cart_items WHERE id = order_id;
-
---   DELETE FROM products WHERE id = v_product_id;
---   DELETE FROM cart_items WHERE id = order_id;
+  DELETE FROM products WHERE id = v_product_id;
+  DELETE FROM cart_items WHERE id = order_id;
   
---   SELECT order_id AS id;
--- END$$
--- DELIMITER ;
+  SELECT order_id AS id;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE add_to_cart(IN product_id INT, IN size VARCHAR(45), IN quantity INT)
+BEGIN
+    INSERT INTO cart_items (product_id, size, quantity) VALUES (product_id, size, quantity);
+    INSERT INTO orders (product_id, size, quantity) VALUES (product_id, size, quantity);
+END $$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE edit_cart(IN item_id INT, IN new_quantity INT, IN new_size VARCHAR(45))
+BEGIN
+    -- Update quantity
+    UPDATE cart_items SET quantity = new_quantity WHERE id = item_id;
+    UPDATE orders SET quantity = new_quantity WHERE id = item_id;
+
+    -- Update size
+    UPDATE cart_items SET size = new_size WHERE id = item_id;
+    UPDATE orders SET size = new_size WHERE id = item_id;
+END $$
+DELIMITER ;
+
+CREATE PROCEDURE `delete_cart_item`(IN item_id INT)
+BEGIN
+    DELETE FROM cart_items WHERE id = item_id;
+    DELETE FROM orders WHERE id = item_id;
+END;
+
+
+--TRIGGERS
+DELIMITER $$
+
+CREATE TRIGGER before_update_cart_items
+BEFORE UPDATE ON amlshop.cart_items
+FOR EACH ROW
+BEGIN
+    DECLARE v_product_id INT;
+    DECLARE v_threshold_quantity INT;
+
+    -- Set the threshold quantity (change as needed)
+    SET v_threshold_quantity = 10;
+
+    -- Get the product_id corresponding to the updated cart_item
+    SELECT product_id INTO v_product_id FROM amlshop.cart_items WHERE id = NEW.id;
+
+    -- Check if the updated quantity exceeds the threshold
+    IF (NEW.quantity >= v_threshold_quantity) THEN
+        -- Your custom logic or actions here (e.g., send notification, update another table, etc.)
+        -- For example, you can print a message to the console
+        SET NEW.quantity = v_threshold_quantity; -- Optionally, set the quantity to the threshold value
+    END IF;
+END $$
+
+DELIMITER ;
